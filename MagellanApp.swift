dd742d3434abe525e1504dce1b32a75fd235ef32
Airborn137 <airborn106@icloud.com>
2025-05-25 21:45:02 +0300

    Initial commit

---
+++ MagellanApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagellanApp.swift
+//  Magellan
+//
+//  Created by Slava Ovchinnikov on 25.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct MagellanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
