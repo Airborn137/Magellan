c2b79d5bf41999d9ae498c864d8abfea104978a4
Airborn137 <airborn106@icloud.com>
2025-06-12 08:22:23 +0300

    Structure

---
+++ New Group/Magellan/MagellanApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagellanApp.swift
+//  Magellan
+//
+//  Created by Slava Ovchinnikov on 12.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct MagellanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
